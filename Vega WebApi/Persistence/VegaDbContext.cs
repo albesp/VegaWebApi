0c7b1338dc7e7665ddd4ae66852cc350ca02cc1e
j <aaa>
2020-05-15 17:29:57 -0500

    New Clases

---
+++ Vega WebApi/Persistence/VegaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega_WebApi.Models;
+
+namespace Vega_WebApi.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+             : base(options)
+        {
+        
+        }
+
+        public DbSet<Make> Makes { get; set; }
+
+    }
+}
